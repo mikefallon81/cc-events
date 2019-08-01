f2f790d798551f532c1440bd596c7b2e8cde6115
lawrie-sm <alertrelic@gmail.com>
2019-08-01 15:01:59 +0100

    Getting form events and outputting and resetting

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit', (e) => {
-    e.preventDefault();
     console.dir(e.target);
+    e.preventDefault();
+    const readingList = document.querySelector('#reading-list');
+    const newDiv = document.createElement('div');
+
+    const titleHeader = document.createElement('h2');
+    titleHeader.textContent = e.target.title.value;
+    const authorEntry = document.createElement('h3');
+    authorEntry.textContent = e.target.author.value;
+    const categoryEntry = document.createElement('h4');
+    categoryEntry.textContent = e.target.category.value;
+
+    newDiv.appendChild(titleHeader);
+    newDiv.appendChild(authorEntry);
+    newDiv.appendChild(categoryEntry);
+    readingList.appendChild(newDiv);
+
+    e.target.reset();
   });
 });
