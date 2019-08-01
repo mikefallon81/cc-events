8aaa74458af8721eebafe07f582c001d0d2c2293
lawrie-sm <alertrelic@gmail.com>
2019-08-01 14:44:43 +0100

    Linked JS, got form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
-})
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.dir(e.target);
+  });
+});
